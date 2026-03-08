1a8faa6bdd0f4428e13c3997431fefae5c1314c1
tiago649 <115178717+tiago649@users.noreply.github.com>
2026-03-08 14:27:54 +0000

    Update WAMT.kt

--- src/main/kotlin/dev/reyaan/wherearemytms/fabric/WAMT.kt
+++ src/main/kotlin/dev/reyaan/wherearemytms/fabric/WAMT.kt
@@ -2,17 +2,14 @@ package dev.reyaan.wherearemytms.fabric
 
 import com.cobblemon.mod.common.CobblemonItems
 import com.cobblemon.mod.common.item.group.CobblemonItemGroups
-import com.cobblemon.mod.common.pokemon.Pokemon
 import dev.reyaan.wherearemytms.fabric.block.tmmachine.TMMachineBlock
 import dev.reyaan.wherearemytms.fabric.block.tmmachine.TMMachineBlockEntity
 import dev.reyaan.wherearemytms.fabric.config.WAMTConfigHandler
 import dev.reyaan.wherearemytms.fabric.config.WAMTConfigObject
 import dev.reyaan.wherearemytms.fabric.item.MoveTransferItem
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.ModifyEntries
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.`object`.builder.v1.block.entity.FabricBlockEntityTypeBuilder
 import net.fabricmc.loader.api.FabricLoader
@@ -23,60 +20,69 @@ import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.block.enums.DoubleBlockHalf
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
-import net.minecraft.item.ItemGroup
-import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import java.io.File
 
+object WAMT : ModInitializer {
 
-object WAMT: ModInitializer {
     const val MOD_ID = "wherearemytms"
 
     fun id(path: String): Identifier {
-        return Identifier(MOD_ID, path)
+        return Identifier.of(MOD_ID, path)
     }
 
     var POKEMON_HM = MoveTransferItem(Item.Settings().maxCount(16), true, "title.wherearemytms.hm")
     var POKEMON_TM = MoveTransferItem(Item.Settings().maxCount(16), false, "title.wherearemytms.tm")
 
-    val TM_MACHINE: Block = TMMachineBlock(FabricBlockSettings.copy(Blocks.SMITHING_TABLE).strength(3.0f).hardness(4.0f).nonOpaque().luminance{ if (it.get(TMMachineBlock.HALF) == DoubleBlockHalf.LOWER) 5 else 0 })
-    val TM_MACHINE_ITEM = BlockItem(TM_MACHINE, FabricItemSettings())
+    val TM_MACHINE: Block = TMMachineBlock(
+        FabricBlockSettings.copy(Blocks.SMITHING_TABLE)
+            .strength(3.0f)
+            .hardness(4.0f)
+            .nonOpaque()
+            .luminance { state ->
+                if (state.get(TMMachineBlock.HALF) == DoubleBlockHalf.LOWER) 5 else 0
+            }
+    )
+
+    val TM_MACHINE_ITEM = BlockItem(TM_MACHINE, Item.Settings())
 
     val TM_MACHINE_BLOCK_ENTITY: BlockEntityType<TMMachineBlockEntity> = Registry.register(
         Registries.BLOCK_ENTITY_TYPE,
         id("move_machine_block_entity"),
-        FabricBlockEntityTypeBuilder.create({ pos: BlockPos, state: BlockState ->
-            TMMachineBlockEntity(
-                pos,
-                state
-            )
-        }, TM_MACHINE).build()
+        FabricBlockEntityTypeBuilder.create(
+            { pos: BlockPos, state: BlockState ->
+                TMMachineBlockEntity(pos, state)
+            },
+            TM_MACHINE
+        ).build()
     )
 
     val TM_MACHINE_MOVE_SELECT_PACKET_ID = id("packet.wherearemytms.close")
 
-
     lateinit var config: WAMTConfigObject
 
     override fun onInitialize() {
-        config = WAMTConfigHandler(File(FabricLoader.getInstance().configDir.toString() + "/wherearemytms.json")).init()
+
+        config = WAMTConfigHandler(
+            File(FabricLoader.getInstance().configDir.toString() + "/wherearemytms.json")
+        ).init()
 
         registerTMs()
         registerMachine()
 
         ItemGroupEvents.modifyEntriesEvent(CobblemonItemGroups.BLOCKS_KEY)
-            .register(ModifyEntries { content: FabricItemGroupEntries ->
+            .register { content: FabricItemGroupEntries ->
                 content.addAfter(
                     CobblemonItems.HEALING_MACHINE,
                     TM_MACHINE_ITEM
                 )
-            })
+            }
 
         ItemGroupEvents.modifyEntriesEvent(CobblemonItemGroups.CONSUMABLES_KEY)
-            .register(ModifyEntries { content: FabricItemGroupEntries ->
+            .register { content: FabricItemGroupEntries ->
                 content.addAfter(
                     CobblemonItems.RARE_CANDY,
                     POKEMON_TM
@@ -85,13 +91,19 @@ object WAMT: ModInitializer {
                     POKEMON_TM,
                     POKEMON_HM
                 )
-            })
+            }
 
         WAMTNetwork.serverListeners()
     }
 
     fun registerMachine() {
-        Registry.register(Registries.BLOCK, id("move_machine"), TM_MACHINE)
+
+        Registry.register(
+            Registries.BLOCK,
+            id("move_machine"),
+            TM_MACHINE
+        )
+
         Registry.register(
             Registries.ITEM,
             id("move_machine"),
@@ -100,7 +112,17 @@ object WAMT: ModInitializer {
     }
 
     fun registerTMs() {
-        Registry.register(Registries.ITEM, id("blank_hm"), POKEMON_HM)
-        Registry.register(Registries.ITEM, id("blank_tm"), POKEMON_TM)
+
+        Registry.register(
+            Registries.ITEM,
+            id("blank_hm"),
+            POKEMON_HM
+        )
+
+        Registry.register(
+            Registries.ITEM,
+            id("blank_tm"),
+            POKEMON_TM
+        )
     }
 }
